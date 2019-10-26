d5eb5e7a8b1f68b0a24512669b10bd06dd2e602e
Hiromu OCHIAI <otiai10@gmail.com>
2019-10-25 21:47:05 -0300

    v0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='amesh',
     packages=find_packages(),
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     description='みんな大好き東京アメッシュ',
     author='Hiromu OCHIAI',
